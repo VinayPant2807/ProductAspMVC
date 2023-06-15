1e30b17e2dd0847be0e46d10c13d541b733841fb
VinayPant2807 <Vinaypant123@gmail.com>
2023-06-15 23:50:47 +0530

    Product CRUD Project

---
+++ Controllers/CategoryMasterController.cs
@@ -0,0 +1,112 @@
+﻿using BhrateApp.Data;
+using BhrateApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+public class CategoryMasterController : Controller
+{
+    private readonly BhrateAppDbContext _context;
+
+    public CategoryMasterController(BhrateAppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Get(int page = 1)
+    {
+        int pageSize = 10;
+
+        int totalRecords = _context.Category_Masters.Count();
+
+        var categories = _context.Category_Masters
+            .OrderBy(c => c.CatMaster_Id)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        int totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+
+        ViewBag.CurrentPage = page;
+        ViewBag.TotalPages = totalPages;
+
+        return View(categories);
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Category_Master category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Category_Masters.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Get");
+        }
+
+        return View(category);
+    }
+
+    [HttpGet]
+    public IActionResult Delete()
+    {
+        var categories = _context.Category_Masters
+            .OrderBy(c => c.CatMaster_Id)
+            .ToList();
+
+        ViewBag.Categories = categories;
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var category = _context.Category_Masters.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        _context.Category_Masters.Remove(category);
+        _context.SaveChanges();
+
+        TempData["Message"] = "Category deleted successfully.";
+
+        return RedirectToAction("Delete");
+    }
+
+    [HttpGet]
+    public IActionResult Update()
+    {
+        var categories = _context.Category_Masters
+            .OrderBy(c => c.CatMaster_Id)
+            .ToList();
+
+        return View(categories);
+    }
+
+    [HttpPost]
+    public IActionResult Update(int id, string categoryName)
+    {
+        var category = _context.Category_Masters.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        category.Cat_Name = categoryName;
+        _context.SaveChanges();
+
+        TempData["UpdateMessage"] = "Category updated successfully.";
+
+        return RedirectToAction("Update");
+    }
+}
---
+++ Data/BhrateAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BhrateApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BhrateApp.Data
+{
+    public class BhrateAppDbContext : DbContext
+    {
+        public BhrateAppDbContext()
+        {
+
+        }
+        public BhrateAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category_Master> Category_Masters { get; set; }
+
+        public DbSet<Product_Master> Product_Masters {get; set; } 
+
+    }
+}
